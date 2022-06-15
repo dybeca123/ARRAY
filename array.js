6bf9dca514df6a00d83ff0368b7859f120b6e456
dybeca123 <103453892+dybeca123@users.noreply.github.com>
2022-06-15 16:17:14 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+let notas = [8, 7, 10]; //array (lista)
+console.log(notas);
+
+console.log(notas[1]); //mostra o conteudo presente em determinada posição do array
+console.log(notas.length); //mostra a quantidade (comprimento) de itens presentes no array
+
+let media = (notas[0] + notas[1] + notas[2]) / notas.length;
+console.log(media);
---
+++ arraySubstituir.js
@@ -0,0 +1,4 @@
+let chamada = ['Leo', 'Davi', 'VK', 'Cris'];
+
+chamada.splice (1,2, 'Nicole'); //substitui
+console.log(chamada);
